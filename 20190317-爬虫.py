f7990037da49ad6e276eeba8a71947dae40ec6e1
cchhgithub <1106088421@qq.com>
2019-05-26 15:47:18 +0800

    爬取番号加图片

--- 20190317-爬虫.py
+++ 20190317-爬虫.py
@@ -1,9 +1,54 @@
-import requests
+import requests,os
 from bs4 import BeautifulSoup
-r=requests.get('http://www.d28k.com/cn/vl_genre.php?g=ne')
-print(r.status_code)
-r.encoding = 'utf-8'
-soup=BeautifulSoup(r.text,"html.parser")
-for list in soup.find_all('div',{'class':'id'}):
-    if not list.string == None:
-        print(list.string)
+
+
+def fanhao():
+    url = 'http://www.d28k.com/cn/vl_genre.php'
+    # http: // www.d28k.com / cn / vl_genre.php?g = cu
+    params = {
+        'mode': '',
+        'g':'cu',
+        'page':'1'
+    }
+    for i in range(1,11):
+        filename = 'fanhao.txt'
+        params['page'] = i
+        res = requests.get(url,params)
+        print(res.url)
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.content,'lxml')
+            fanhaoList = soup.find_all('div',class_ = 'video')
+            for item in fanhaoList:
+                with open(filename,'a',encoding='utf-8') as w:
+                    w.write(item.string+'   ')
+            with open(filename,'a',encoding='utf-8') as w2:
+                w2.write('\n')
+
+def tupian():
+    url = 'http://www.d28k.com/cn/vl_genre.php'
+    # http: // www.d28k.com / cn / vl_genre.php?g = cu
+    params = {
+        'mode': '',
+        'g': 'cu',
+        'page': '1'
+    }
+    res = requests.get(url, params)
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.content,'lxml')
+        fanhaolist = soup.find_all('div', class_='video')
+        for item in fanhaolist:
+            fanhaoTag = item.find('div',class_='id')
+            fanhao = fanhaoTag.string
+            pictureTag = item.find('img')
+            srcUrl = pictureTag['src']
+            srcUrl = 'http:'+srcUrl
+            r = requests.get(srcUrl)
+            targetDir = 'picture'
+            filename = os.path.join(targetDir,fanhao+'.jpg')
+            with open(filename,'wb') as f:
+                f.write(r.content)
+
+
+if __name__ == '__main__':
+    # fanhao()
+    tupian()
